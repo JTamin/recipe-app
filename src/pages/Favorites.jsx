af261873b407c33e267d14003fdf45cd72e09dee
Jtamin <taminjus33@email.com>
2026-03-25 10:27:11 +0800

    fix: spa routing on vercel

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,19 +1,23 @@
-import RecipeCapsule from "../components/RecipeCapsule";
-import { useFavContext } from "../context/favoriteContext";
 import { Helmet } from "react-helmet";
+import { useFavContext } from "../context/favoriteContext";
 
 export const Favorites = () => {
-  const { favorite, toggleFavorite } = useFavContext();
+  const { favorite } = useFavContext();
 
   return (
-    <div className="flex flex-col  items-center  gap-6 p-4 w-full">
+    <div className="flex flex-col items-center gap-6 p-4 w-full">
       <Helmet>
         <title>Favorite Page</title>
         <meta name="description" content="Browse your Favorites" />
       </Helmet>
-      {favorite.map((item, index) => (
-        <RecipeCapsule key={item.id} item={item} index={index} />
-      ))}
+
+      {favorite.length === 0 ? (
+        <h1 className="text-white text-2xl">No Favorites found</h1>
+      ) : (
+        favorite.map((item, index) => (
+          <RecipeCapsule key={item.id} item={item} index={index} />
+        ))
+      )}
     </div>
   );
 };
